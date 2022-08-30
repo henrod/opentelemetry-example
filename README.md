8eaff842970548289c4a6fa70c34c442977d0f82
Henrique Rodrigues <henrique.rodrigues@wildlifestudios.com>
2022-08-29 21:51:45 -0300

    Simplify to only run Jaeger

--- README.md
+++ README.md
@@ -8,6 +8,18 @@ Write an example on how to use OpenTelemetry with:
 3. Postgres Client
 4. Redis Client
 
-Traces can be found in:
-1. [Jaeger UI](localhost:16686) if Jaeger is used.
-2. [traces.txt](traces.txt) if file is used.
+## Running
+
+Start Jaeger:
+
+```shell
+make deps
+```
+
+Run the application:
+
+```shell
+make run
+```
+
+Find the traces in [Jaeger UI](localhost:16686).
--- traces.go
+++ traces.go
@@ -2,28 +2,22 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"os"
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/jaeger"
-	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
 )
 
-const (
-	ExporterTypeFile = iota
-	ExporterTypeJaeger
-)
+const jaegerURL = "http://localhost:14268/api/traces"
 
 func ConfigureTracer() (func() error, error) {
-	exporter, closeExporter, err := getExporter(ExporterTypeJaeger)
+	exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(jaegerURL)))
 	if err != nil {
-		return nil, fmt.Errorf("failed to get exporter: %w", err)
+		return nil, fmt.Errorf("failed to create jaeger exporter: %w", err)
 	}
 
 	traceResource, _ := resource.Merge(
@@ -49,51 +43,8 @@ func ConfigureTracer() (func() error, error) {
 			return fmt.Errorf("failed to shutdown tracer provider: %w", err)
 		}
 
-		err = closeExporter()
-		if err != nil {
-			return fmt.Errorf("failed to close exporter: %w", err)
-		}
-
 		return nil
 	}
 
 	return closeFunction, nil
-}
-
-func getExporter(exporterType int) (trace.SpanExporter, func() error, error) {
-	switch exporterType {
-	case ExporterTypeFile:
-		tracesFile, err := os.Create("traces.txt")
-		if err != nil {
-			return nil, nil, fmt.Errorf("faield to open traces file: %w", err)
-		}
-
-		exporter, err := stdouttrace.New(
-			stdouttrace.WithWriter(tracesFile),
-			stdouttrace.WithPrettyPrint(),
-		)
-		if err != nil {
-			return nil, nil, fmt.Errorf("failed to create trace exporter: %w", err)
-		}
-
-		return exporter, func() error {
-			err = tracesFile.Close()
-			if err != nil {
-				return fmt.Errorf("failed to close traces file: %w", err)
-			}
-
-			return nil
-		}, nil
-	case ExporterTypeJaeger:
-		exporter, err := jaeger.New(
-			jaeger.WithCollectorEndpoint(jaeger.WithEndpoint("http://localhost:14268/api/traces")),
-		)
-		if err != nil {
-			return nil, nil, fmt.Errorf("failed to create jaeger exporter: %w", err)
-		}
-
-		return exporter, func() error { return nil }, nil
-	}
-
-	return nil, nil, errors.New("invalid exporter type")
 }
